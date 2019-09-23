5f884f52631bee6b9f562b039fd572370b4703e9
Phạm Văn Điệp <diep.job@outlook.com>
2019-09-23 12:08:04 +0700

    finish to switch add company chat

--- src/Resources/Message.php
+++ src/Resources/Message.php
@@ -16,7 +16,7 @@ class Message extends Item
             'id'         => $this->_id,
             'body'       => $this->body,
             'type'       => $this->type,
-            'attachments'=> in_array($this->type,['image', 'video', 'audio', 'file']) && $this->attachments != null ? new AttachableCollection( $this->attachments()->simplePaginate(50) ) : [],
+            'attachments'=> in_array($this->type,['image', 'video', 'audio', 'file','application']) && $this->attachments != null ? new AttachableCollection( $this->attachments()->simplePaginate(50) ) : [],
             'createdAt'  => $this->created_at,
             'updatedAt'  => $this->updated_at,
             'author'     => new User( $this->author ),
